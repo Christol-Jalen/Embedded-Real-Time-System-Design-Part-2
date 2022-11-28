5017772a950b134763944570eb0e25662be8ec55
Yujie Wang <79264663+Christol-Jalen@users.noreply.github.com>
2022-11-28 13:03:41 +0000

    initial commit
    TODO:
    1. finish the last function
    2. for loop

---
+++ ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/Debug/inc/songFile.d
@@ -0,0 +1,186 @@
+# FIXED
+
+inc/songFile.obj: ../inc/songFile.c
+inc/songFile.obj: ../inc/songFile.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/msp.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/msp432p401r.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/msp_compatibility.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/msp432p401r_classic.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+inc/songFile.obj: C:/TI/ccs/ccs_base/arm/include/system_msp432p401r.h
+inc/songFile.obj: ../inc/CortexM.h
+inc/songFile.obj: ../inc/SysTick.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/FreeRTOS.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/FreeRTOSConfig.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/driverlib.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/adc14.h
+inc/songFile.obj: C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/aes256.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/comp_e.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/cpu.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/crc32.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/cs.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/dma.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/eusci.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/flash.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/fpu.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/gpio.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/i2c.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/interrupt.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/mpu.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/pcm.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/pmap.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/pss.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/ref_a.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/reset.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/rom.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/rom_map.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/rtc_c.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/spi.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/sysctl.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/systick.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/timer32.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/timer_a.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/uart.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/wdt_a.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/projdefs.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/portable.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/deprecated_definitions.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h
+inc/songFile.obj: C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/mpu_wrappers.h
+
+../inc/songFile.c:
+
+../inc/songFile.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+C:/TI/ccs/ccs_base/arm/include/msp.h:
+
+C:/TI/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/TI/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/TI/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/TI/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/TI/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/TI/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/TI/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
+../inc/CortexM.h:
+
+../inc/SysTick.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/FreeRTOSConfig.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/driverlib.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/adc14.h:
+
+C:/TI/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/aes256.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/comp_e.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/cpu.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/crc32.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/cs.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/dma.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/eusci.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/flash.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/fpu.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/gpio.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/i2c.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/interrupt.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/mpu.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/pcm.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/pmap.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/pss.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/ref_a.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/reset.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/rom.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/rom_map.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/rtc_c.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/spi.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/sysctl.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/systick.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/timer32.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/timer_a.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/uart.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/CCS/TI_MSP432/driverlib/wdt_a.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/projdefs.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/portable.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h:
+
+C:/Users/ho19623/OneDrive\ -\ University\ of\ Bristol/Documents/Downloads/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/ERTS_34030_sw_part_2/FreeRTOS/Source/include/mpu_wrappers.h:
+
